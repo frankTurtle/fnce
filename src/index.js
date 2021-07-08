e0823e7be84e3b4a77daa24ba9e80b18424fdd60
Barret J. Nobel <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-07-08 15:46:07 -0400

    initial structure up and running (#1)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Hello!')
+const com = require("commander");
+const inquirer = require("inquirer");
+
+com
+.version('0.0.1')
+.command('init')
+.action( options => {
+    const questions = [
+        { type: 'input', name: 'firstName', message: 'Enter your first name'},
+        { type: 'input', name: 'annualSalary', message: 'Enter your Annual Salary'}
+    ];
+
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+        const { firstName, annualSalary } = answers;
+        const salaryNum = parseInt(annualSalary);
+
+        console.log(`First: ${firstName} - Salary: $${salaryNum}`);
+        process.exit();
+    }).catch( err => {
+        console.log(err);
+        process.exit();
+    });
+});
+
+com.parse(process.argv);
