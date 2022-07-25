6de29078232de55dd0c58389fae1b7d780610f38
frankTurtle.crypto <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2022-07-24 21:43:37 -0400

    Feature/auth (#3)
    * chore: cleaned up login page UI
    
    * fix: updated gitignore for environment
    
    * feat: ability to login / logout / account creation
    
    * fix: refactored the auth
    
    * fix: escaped characters linting error

---
+++ financial-boss-web-app/firebase/config.js
@@ -0,0 +1,31 @@
+// import { FirebaseApp, initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getDatabase } from "firebase/database";
+
+// const CLIENT_CONFIG = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// };
+
+// FirebaseApp.initializeApp(CLIENT_CONFIG);
+
+// export default FirebaseApp;
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const CLIENT_CONFIG = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+export const firebase = initializeApp(CLIENT_CONFIG);
+export const auth = getAuth(firebase);
